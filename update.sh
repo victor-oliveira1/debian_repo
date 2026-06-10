b3da00150a8ce7fcf44e604745af30549f7fca3d
Victor Oliveira <victor.oliveira@semeartech.com.br>
2026-06-10 14:49:04 -0300

    Updated again

--- update.sh
+++ update.sh
@@ -3,4 +3,8 @@ dpkg-scanpackages . /dev/null > Packages
 apt-ftparchive release . > Release
 git add .
 git commit
-read -p 'Send to Github? (y/n): '
+read -p 'Send to Github? (y/n): ' response
+case $response in
+    y)
+    git push
+esac
